258b592cca2c97085feb0bfbac5528b142c5c462
Junaid B <juni@Junaids-MacBook-Pro.local>
2026-02-28 15:09:57 -0500

    Remove .terraform folder from repo to fix large file error

--- junaid-blog/terraform-js/state.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "jb-my-terraform-state-file"
-        key = "global/s3/terraform.tfstate"
-        region = "us-east-1"
-        dynamodb_table = "terraform-lock-file-db"
-
-    }
-}
